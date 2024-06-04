d25374a3d991ceaf49bc90c4c72b44147b70dee3
Khalil Elhajoui <khalil_elhajoui@MacBook-Pro-de-Khalil.local>
2024-06-04 15:06:37 +0200

    updated the file perceptron.py whith all the function for the single layer perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,30 +1,56 @@
 import numpy as np
 
+
+#This function creat a matrice with a 'w' coefficent for each x parameters the datas have
+
 def initialisation(X):
     W = np.random.randn(X.shape[1],1)
-    b = np.random.randn(1)
+    b = np.random.randn()
     return (W,b)
+
+
+#This function modelize our decision making model
+    #The Z is our linear model
+    #The A is the sigmoid function who give us more precition under 0.5 the data is from class 0 over 0.5 is from class 1
+
 
 def model(X,W,b):
     Z = X.dot(W)+b
     A = 1/(1+np.exp(-Z))
     return A
 
+
+#This function calculate the precision of the model
+    #A is the model prediction
+    #Y are the true answers
+
 def cost(A,Y):
-    L = -1/(Y.shape[0])*np.sum(Y*np.log(A)+(1-Y)*np.log(1-A))
+    L = -1/len(Y)*np.sum(Y*np.log(A)+(1-Y)*np.log(1-A))
     return L
 
+def cost1(A, Y):
+    L = 0
+    for i in range(len(Y)):
+        L += Y[i] * np.log(A[i]) + (1 - Y[i]) * np.log(1 - A[i])
+    Lf = -1 / len(Y) * L
+    return Lf
+
+#This function calculate the delta who will permit us to update our coefficiant
+
 def gradients(A,X,Y):
-    dW = 1/Y.shape[0]*(X.T).dot(A-Y)
-    db = 1/Y.shape[0]*np.sum(A-Y)
+    dW = 1/len(Y)*(X.T).dot(A-Y)
+    db = 1/len(Y)*np.sum(A-Y)
     return dW,db
+
+
+#With this function we update the functions
 
 def updatecoeff(W,b,dW,db,alpha):
     W = W-alpha*dW
     b = b-alpha*db
     return W,b
 
-
+#This function is the loop that train the model it return us out parameter that we can save like that we dosen't have to train the model each time we want to use it
 def perceptron(X,Y,alpha):
     seuil=int(input("Entrez un nombre d'itteration"))
     W,b = initialisation(X)
@@ -45,10 +71,10 @@ def prediction(X,W,b):
 
 
 data_and = np.array([[1,0],[1,1],[1,0]])
-yt_and = np.array([0,1,0])
+yt_and = np.array([[0],[1],[0]])
 
 W,b,Loss = perceptron(data_and,yt_and,0.5)
 
-x1=np.array([[1,1]])
+x1=np.array([[0,0]])
 
 print(prediction(x1,W,b))
